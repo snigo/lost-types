e693afb57640636dee71e4a787b05a7f46785d78
snigo <snigo.ogins@gmail.com>
2020-05-29 18:17:23 +0200

    Add range class

---
+++ packages/range/range.d.ts
@@ -0,0 +1,17 @@
+declare class Range {
+  constructor(rangeStart: number, rangeEnd: number, step?: number);
+  static from(iterableNumbers: any): Range;
+  get length(): number;
+  get max(): number;
+  get min(): number;
+  get center(): number;
+  forEach(fn: Function, step?: number): void;
+  forEachReverse(fn: Function, step?: number): void;
+  has(number: number): boolean;
+  clamp(number: number): number;
+  toArray(): number[];
+  getFraction(number: number, precision?: number): number;
+  fromFraction(number: number, precision?: number): number;
+  slice(parts: number): number[];
+  mod(number: number): number;
+}
